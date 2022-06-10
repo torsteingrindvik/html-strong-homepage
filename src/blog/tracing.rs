cddfd9542a56f744471e0168d947a948c2de0c81
Torstein Grindvik <torstein.grindvik@nordicsemi.no>
2022-06-10 20:13:15 +0200

    WIP tracing. Surely we will fixup! this commit later
    Signed-off-by: Torstein Grindvik <torstein.grindvik@nordicsemi.no>

--- src/blog/tracing.rs
+++ src/blog/tracing.rs
@@ -1,5 +1,6 @@
 use axum::Extension;
-use axum::{extract::Path, response::Html, routing::get, Router};
+use axum::{response::Html, routing::get, Router};
+use html_strong::document_tree::Node;
 use html_strong::{science_lab::NodeExt, tags::*};
 use reqwest::StatusCode;
 
@@ -7,10 +8,232 @@ use crate::{Base, ContentUrl};
 
 use super::{blog_page, BlogEntry, Series, State};
 
+enum Posts {
+    Overview,
+}
+
+impl Posts {
+    fn url(&self) -> &'static str {
+        match self {
+            Posts::Overview => "/overview",
+        }
+    }
+
+    fn title(&self) -> &'static str {
+        match self {
+            Posts::Overview => "2022-06-04: Overview",
+        }
+    }
+
+    // TODO: English name?
+    fn leder(&self) -> &'static str {
+        match self {
+            Posts::Overview => {
+                "Hello world/tracing! Let's get an overview of what tracing is and why we'd want \
+                 to use it."
+            }
+        }
+    }
+}
+
 pub fn router() -> Router {
     Router::new()
         .route("/", get(landing))
-        .route("/:sub", get(sub))
+        .route(Posts::Overview.url(), get(overview))
+}
+
+#[derive(Debug)]
+enum Tidbit {
+    Text(String),
+    Url { url: String, text: String },
+    Image(String),
+    Code(String),
+    Shell(String),
+}
+
+#[derive(Debug)]
+struct Paragraph {
+    tidbits: Vec<Tidbit>,
+}
+
+impl Paragraph {
+    fn new() -> Self {
+        Self { tidbits: vec![] }
+    }
+
+    fn push(&mut self, tidbit: Tidbit) {
+        self.tidbits.push(tidbit);
+    }
+}
+
+#[derive(Debug)]
+struct Section {
+    title: String,
+    paragraphs: Vec<Paragraph>,
+}
+
+impl Section {
+    fn new(title: &'static str) -> Self {
+        Self {
+            title: title.into(),
+            paragraphs: vec![],
+        }
+    }
+}
+
+impl NodeExt for Section {
+    fn into_node(self) -> Node {
+        let mut output = Div.class("blog-section");
+
+        // We always start out a section with some title.
+        output.push_kid(H2.text(self.title));
+
+        for paragraph in self.paragraphs {
+            let mut to_render: Option<Node> = None;
+
+            for tidbit in paragraph.tidbits {
+                let to_add: Node = match tidbit {
+                    Tidbit::Url { url, text } => A::href(&url).text(text),
+                    Tidbit::Image(path) => Img::new(&path).into_node(),
+                    Tidbit::Code(code) => Code.class("blog-code rounded").text(code).into_node(),
+                    Tidbit::Shell(command) => {
+                        Code.class("blog-shell rounded").text(command).into_node()
+                    }
+                    Tidbit::Text(text) => {
+                        to_render = Some(to_render.get_or_insert(P.into_node()).text(text));
+
+                        continue;
+                    }
+                };
+
+                to_render.as_mut().unwrap().push_kid(to_add);
+            }
+
+            output.push_kid(to_render.unwrap());
+        }
+
+        output
+    }
+}
+
+#[derive(Debug)]
+struct Article {
+    sections: Vec<Section>,
+}
+
+impl Article {
+    fn new() -> Self {
+        Self { sections: vec![] }
+    }
+
+    fn add_tidbit(mut self, tidbit: Tidbit) -> Self {
+        self.sections
+            .last_mut()
+            .unwrap()
+            .paragraphs
+            .last_mut()
+            .unwrap()
+            .push(tidbit);
+        self
+    }
+
+    /// This starts a new paragraph with the given text.
+    fn p(mut self, text: &'static str) -> Self {
+        self.sections
+            .last_mut()
+            .unwrap()
+            .paragraphs
+            .push(Paragraph::new());
+        self.text(text)
+    }
+
+    /// This adds more text to an ongoing paragraph.
+    fn text(self, text: &'static str) -> Self {
+        self.add_tidbit(Tidbit::Text(text.into()))
+    }
+
+    fn code(self, text: &'static str) -> Self {
+        self.add_tidbit(Tidbit::Code(text.into()))
+    }
+
+    fn image(self, text: &'static str) -> Self {
+        self.add_tidbit(Tidbit::Image(text.into()))
+    }
+
+    /// This adds an inline shell-like command to an ongoing paragraph.
+    fn shell(self, text: &'static str) -> Self {
+        self.add_tidbit(Tidbit::Shell(html_escape::encode_text(text).to_string()))
+    }
+
+    /// This adds an inline url to an ongoing paragraph.
+    fn url(self, url: &'static str, text: &'static str) -> Self {
+        self.add_tidbit(Tidbit::Url {
+            url: url.into(),
+            text: text.into(),
+        })
+    }
+
+    /// This starts a new section with the given header.
+    fn header(mut self, text: &'static str) -> Self {
+        self.sections.push(Section::new(text));
+        self
+    }
+}
+
+impl NodeExt for Article {
+    fn into_node(self) -> Node {
+        let mut output = Div.class("blog-article");
+
+        for section in self.sections {
+            output.push_kid(Div.class("breather-y").kid(section));
+        }
+
+        output
+    }
+}
+
+fn overview_impl() -> impl NodeExt {
+    Article::new()
+        .header("Tracing: An overview")
+        .p("Tokio ")
+        .url("https://github.com/tokio-rs/tracing", "tracing")
+        .text(
+            " looks really neat. I've sporadically used it for various small projects, \
+             with various degrees of success. Most times I just use it to get the various \
+             standard log macros, and rely on ",
+        )
+        .shell("RUST_LOG=<some log level>")
+        .text(" to make things show up in my terminal.")
+        .p("But there seems to be a lot more to the story!")
+        .p("Let's lay out some goals if what to figure out. Note that at the time of writing, I don't know the answers, and as such the goals might not be accurate until I know enough to refine them.")
+        .header("Goals")
+        .p("There are several goals I'd like to achieve by investigating tracing. The goals can be split into a few categories, so let's do that.")
+        .header("Goal: Understanding tracing itself")
+        .p("I'd like to understand the concepts better.")
+        .p("* What is a subscriber?")
+        .p("* Can I use many subscribers at the same time? Should I?")
+        .p("* How does writing my own subscriber look like?")
+        .p("* What is an event?")
+        .p("* What is a span?")
+        .p("* How can the act of entering a span impact code in the same scope? In other words, how can one part of code introduce spooky state on following code?")
+        .p("* Why do we have to be careful with spans when using async?")
+        .p("* What does the instrument attribute do, and what does the instrument call on a future do?")
+        .p("* How is global state managed? Specifically, how does logging interact with a global subscriber?")
+        .header("Practical goal: Making distributed traces")
+        .p("I want to know how I can make an application where I have some servers with clients talking to each server, and end up with traces which are scoped in such a way that it's easy to see what happened during each connection.")
+        .p("Also, let's say we have 10 000 of the above sessions. Can I have little/no verbosity for happy connections, and higher verbosity if an error occurs in misbehaving connections?")
+        .header("Goal: The relationship with OpenTelemetry")
+        .p("For having this neat overview of distributed traces, I think ")
+        .url( "https://opentelemetry.io/docs/", "OpenTelemetry")
+        .text(" is a good way to go about things. There seems to be some nice integrations between tracing and OpenTelemetry. But OpenTelemetry too has concepts which I know little of. So...")
+        .p("* How does OpenTelemetry work?")
+        .p("* How well does it map to tracing?")
+        .p("* Which collectors should I use?")
+        .p("* Is a collector what I think it is: The thing which receives traces? Is it very close to being a (tracing) subscriber?")
+}
+
+async fn overview() -> Result<Html<String>, (StatusCode, String)> {
+    blog_page(overview_impl().into_node()).await
 }
 
 pub async fn landing(
@@ -19,18 +242,16 @@ pub async fn landing(
     let mut url = ContentUrl::new(Base::Blog);
     url.dive("tracing");
 
-    let state = state.get(&Series::Tracing).unwrap().clone();
-
+    let tracing_post = Posts::Overview;
     let tracing = BlogEntry::new(
-        &url.suburl("2022-06-04"),
+        &url.suburl(tracing_post.url()),
         super::EntryState {
-            name: "2022-06-04".into(),
-            description: "Hello world/tracing! Let's get an overview of what tracing is and why \
-                          we'd want to use it."
-                .into(),
+            name: tracing_post.title().into(),
+            description: tracing_post.leder().into(),
         },
     );
 
+    let state = state.get(&Series::Tracing).unwrap().clone();
     let contents = Div
         .kid(H1.text(state.name))
         .kid(P.text(state.description))
@@ -38,11 +259,4 @@ pub async fn landing(
         .kid(Div.class("breather-y").kid(tracing));
 
     blog_page(contents).await
-}
-
-pub async fn sub(
-    Path(sub): Path<String>,
-    Extension(state): Extension<State>,
-) -> Result<Html<String>, (StatusCode, String)> {
-    blog_page(H1.text(&format!("Yah: {} + {sub}, heh: {state:?}", "hi"))).await
 }
